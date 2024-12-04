1c45a2b5534990cae9ff33496d787cd68cdb2c77
Christian Abraham Sánchez Sánchez <chrisnewgear2011@gmail.com>
2024-12-04 09:07:50 -0500

    Uncheking selected vendors is working

--- lib/models/usuarios.dart
+++ lib/models/usuarios.dart
@@ -1,55 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-// class Usuario {
-//   final String nombres;
-//   final String apellidos;
-//   final String email;
-//   final String telefono;
-//   final String role;
-//   final String codigo;
-//   final DateTime fechaIngreso;
-
-//   Usuario({
-//     required this.nombres,
-//     required this.apellidos,
-//     required this.email,
-//     required this.telefono,
-//     required this.role,
-//     this.codigo = '',
-//     required this.fechaIngreso,
-//   });
-
-//   factory Usuario.fromFirestore(DocumentSnapshot doc) {
-//     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-
-//     return Usuario(
-//       nombres: data['Nombre'] ?? '',
-//       apellidos: data['Apellidos'] ?? '',
-//       email: data['email'] ?? '',
-//       telefono: data['Telefono'] ?? '',
-//       role: data['Role'] ?? 'None',
-//       codigo: data['Codigo'] ?? '',
-//       fechaIngreso: data['createdAt'] != null
-//           ? (data['createdAt'] as Timestamp).toDate()
-//           : DateTime.now(),
-//     );
-//   }
-
-//   /// Método para convertir la clase Usuario en un mapa compatible con Firebase
-//   Map<String, dynamic> toMap() {
-//     return {
-//       'Nombre': nombres,
-//       'Apellidos': apellidos,
-//       'email': email,
-//       'Telefono': telefono,
-//       'Role': role,
-//       'Codigo': codigo,
-//       'createdAt': fechaIngreso,
-//     };
-//   }
-// }
-
-
 class Usuario {
   final String nombres;
   final String apellidos;
